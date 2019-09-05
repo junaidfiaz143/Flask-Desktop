757a1c96456f0faa1a90fb2d5681da09b47c39b6
jd <junaidfiaz143@gmail.com>
2019-09-05 23:03:34 +0500

    add exit prompt dialog

--- web_app.py
+++ web_app.py
@@ -1,15 +1,33 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+from win32api import GetSystemMetrics
+
+# print("Width =", GetSystemMetrics(0))
+# print("Height =", GetSystemMetrics(1))
 
 class MyApp(QWidget):
 
 	def __init__(self):
 		super().__init__()
 
+
 		self.initUI()
+
+	def closeEvent(self, event):
+
+		QMessageBox.move(self, GetSystemMetrics(1)//2, 0)
+
+		quit_msg = "Are you sure you want to exit the program?"
+		reply = QMessageBox.question(self, 'Message', quit_msg, QMessageBox.Yes, QMessageBox.No)
+
+		if reply == QMessageBox.Yes:
+			event.accept()
+		else:
+			event.ignore()
 
 	def initUI(self):
 
@@ -20,6 +38,8 @@ class MyApp(QWidget):
 		self.web.showMaximized()
 		# self.web.setFixedSize(1366, 768)
 		self.web.show()
+
+		self.web.closeEvent = self.closeEvent
 
 if __name__ == '__main__':
 	
